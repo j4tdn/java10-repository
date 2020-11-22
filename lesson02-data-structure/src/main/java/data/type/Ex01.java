08736ed10dde26ff81be3ca56799780fd464945f
Bao Duong <hoadafh@gmail.com>
2020-11-22 21:56:17 +0700

    lesson02-data-structure22.11.20

---
+++ lesson02-data-structure/src/main/java/data/type/Ex01.java
@@ -0,0 +1,17 @@
+package data.type;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		int i1 = 10;
+		char c1 = 'c';
+		long l1 = 100;
+		float f1 = 10.7f;
+		double d1 = 8.6d;
+		System.out.println("Số nguyên: " + i1);
+		Integer number = 10;
+		Integer oddNumber = 7;
+		String studentName = "Duong Quoc Bao";
+		System.out.println("Integer: " + number);
+	}
+
+}
