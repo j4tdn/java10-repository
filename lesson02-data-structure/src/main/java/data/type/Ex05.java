12c6b9835d662a31300263aa08efb0a7ce2a930e
unknown <Dang>
2020-11-22 21:56:58 +0700

    lesson02-data-structure 22.11.20

---
+++ lesson02-data-structure/src/main/java/data/type/Ex05.java
@@ -0,0 +1,16 @@
+package data.type;
+
+import bean.HandInteger;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		HandInteger a= new HandInteger(10);
+		HandInteger b= new HandInteger(20);
+		
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+
+	}
+
+}
