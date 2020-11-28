4bb59fc2edcbb5ed5eb5c47496f393d7358b65cc
thenguyen <0984894089n@gmail.com>
2020-11-28 22:09:48 +0700

    lesson02-thenguyen-28.11.2020

---
+++ lesson02-thenguyen/src/main/java/DangTheNguyen/lesson02_thenguyen/App.java
@@ -0,0 +1,13 @@
+package DangTheNguyen.lesson02_thenguyen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
