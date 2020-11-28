b252d2df3ae8298e442a1cd3e7ffc0cbc5aaef8c
taquocthang <tathang0330@gmail.com>
2020-11-28 23:02:46 +0700

    lesson02-quocthang-28/11/2020

---
+++ lesson02-quocthang/src/main/java/quocthang/lesson02_quocthang/App.java
@@ -0,0 +1,13 @@
+package quocthang.lesson02_quocthang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
