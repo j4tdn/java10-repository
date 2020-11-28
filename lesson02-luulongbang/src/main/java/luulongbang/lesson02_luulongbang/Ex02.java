502db9054aae48f9186d37a945128cd3364a3791
bang <longbangluu@gmail.com>
2020-11-28 18:20:04 +0700

    lesson02-luulongbang 27.11.2020

--- lesson02-luulongbang/src/main/java/luulongbang/lesson02_luulongbang/Ex02.java
+++ lesson02-luulongbang/src/main/java/luulongbang/lesson02_luulongbang/Ex02.java
@@ -1,5 +1,29 @@
 package luulongbang.lesson02_luulongbang;
 
 public class Ex02 {
-
+	 public static void main(String[] args) {
+		 
+         int a = 4;
+         int b = 7;
+         int c = 12;
+         int d = 18;
+         
+         
+         long mySum= factorial(a) + factorial(b) + factorial(c) + factorial(d) ;	           
+         System.out.println("Sum of three factorials");
+         System.out.println(mySum);
+     }
+	 public static long factorial(int n) {
+         if (n > 0) {
+     
+        	 long prod = 1;
+        	 for (int i =1; i <=n; i++) {
+        		 prod = prod * i;
+        	 }
+             return prod;
+         } else {
+             return 1;
+         }
+     }
 }
+  
