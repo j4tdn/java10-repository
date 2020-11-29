c9de01449cc0630e4544a5aadad1804587b38f47
huuphuc <phuctrana3k58@gmail.com>
2020-11-29 21:54:11 +0700

    lesson02-tranlehuuphuc 29.11.2020

---
+++ lesson02-tranlehuuphuc/src/test/java/tranlehuuphuc/lesson02_tranlehuuphuc/AppTest.java
@@ -0,0 +1,20 @@
+package tranlehuuphuc.lesson02_tranlehuuphuc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
