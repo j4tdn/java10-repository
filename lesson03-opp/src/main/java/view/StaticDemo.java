35df1403128fedd0f13d66e64edffc4763689755
phuquoc2109 <46041877+phuquoc2109@users.noreply.github.com>
2020-11-29 21:47:17 +0700

    lesson03-oop 29.11.2020

---
+++ lesson03-opp/src/main/java/view/StaticDemo.java
@@ -0,0 +1,35 @@
+package view;
+
+public class StaticDemo {
+	private String id;
+	private String name;
+	private static int age;
+	
+	public static void main(String[] args) {
+		// 1, Teo, 18
+		// 2, Nam, 28
+		StaticDemo sd1 = new StaticDemo();
+		sd1.id = "1";
+		sd1.name ="Teo";
+		sd1.age = 18;
+		System.out.println(sd1);
+		
+		StaticDemo sd2 = new StaticDemo();
+		sd2.id = "2";
+		sd2.name ="Nam";
+		sd2.age = 28;
+		System.out.println(sd2);
+	}
+	
+	// methods, attributes
+	// static: class'scope
+	// => construct at the runtime beginning
+	
+	// non-static: object'scope
+	// => will be constructed when object calls
+	
+	@Override
+	public String toString() {
+		return id + ", " + name + ", " +age;
+	}
+}
