21f1bc9367d27aea8918d391b8c189777bcada94
bang <longbangluu@gmail.com>
2020-11-29 21:46:12 +0700

    lesson03-oop 29.11.2020

---
+++ lesson03-oop/src/main/java/view/Shop.java
@@ -0,0 +1,28 @@
+package view;
+
+public class Shop {
+    public static void main(String[] args) {
+    	// initial car object
+    	Car car1 = new Car();
+    	car1.setColor("Green");
+    	car1.setId("C1");
+    	
+    	Car car2 = new Car();
+    	car2.setColor("Yellow");
+    	car2 = car1;
+    	car2.setId("C2");
+    	
+    	Car car3 = new Car("C3", "Black", "Toyota", 1000);
+    	
+    	
+    	// print object => call toString
+    	// everyclass in Java extends class Object
+    	System.out.println(car1);
+    	System.out.println(car2);
+    	System.out.println(car3);
+    	
+    	// . goi phuong thuc do
+    	
+    	
+    }
+}
