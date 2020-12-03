9e3c0db401c1d9389d57b30525cadf834921563d
taquocthang <tathang0330@gmail.com>
2020-12-04 01:39:22 +0700

    lesson03-oop 04.12.2020

---
+++ lesson03-oop/src/main/java/exercises/Customer.java
@@ -0,0 +1,60 @@
+package exercises;
+
+public class Customer {
+	private String cardId;
+	private String fullName;
+	private String phone;
+	private String address;
+	
+	public Customer() {
+	}
+
+	public Customer(String cardId, String fullName, String phone, String address) {
+		super();
+		this.cardId = cardId;
+		this.fullName = fullName;
+		this.phone = phone;
+		this.address = address;
+	}
+
+	public String getCardId() {
+		return cardId;
+	}
+
+	public void setCardId(String cardId) {
+		this.cardId = cardId;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [cardId=" + cardId + ", fullName=" + fullName + ", phone=" + phone + ", address=" + address
+				+ "]";
+	}
+	
+	
+	
+}
