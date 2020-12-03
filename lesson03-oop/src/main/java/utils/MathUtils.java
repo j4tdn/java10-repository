1207c0a0fbc24734727b652f173bdd8311334f26
phuongvanvo <phuongvanvo1610@gmail.com>
2020-12-04 01:13:17 +0700

    lesson03-opp-04.12.2020

---
+++ lesson03-oop/src/main/java/utils/MathUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class MathUtils {
+	private MathUtils() {
+		
+	}
+	// static | non-static
+	public static int sum(int a, int b) {
+
+		return a + b;
+	}
+
+}
---
+++ lesson03-oop/src/main/java/view/MathDemo.java
@@ -0,0 +1,14 @@
+package view;
+
+import utils.MathUtils;
+
+public class MathDemo {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int result = MathUtils.sum(a, b);
+		System.out.println(result);
+	}
+}
+//khi nao dung static : dung khi to utility class
+// 1 class ho tro phuong thuc , cung cap truong input vao tu input do tinh toan trar ket qua maf ko phu thuoc vao doi tuong de khoi khoi tao vung nho thua trne HEAL
