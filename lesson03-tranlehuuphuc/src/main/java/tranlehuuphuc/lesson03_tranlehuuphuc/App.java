b48e49728dacc2038bddefc42aefa003a7aad523
huuphuc <phuctrana3k58@gmail.com>
2020-12-05 02:36:11 +0700

    lesson03-tranlehuuphuc 05.12.2020

---
+++ lesson03-tranlehuuphuc/src/main/java/tranlehuuphuc/lesson03_tranlehuuphuc/App.java
@@ -0,0 +1,13 @@
+package tranlehuuphuc.lesson03_tranlehuuphuc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
