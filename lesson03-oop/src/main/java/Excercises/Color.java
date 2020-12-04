4c54a23483a625962705dfe198b223145c595ff7
duonghuy1123 <duoonghuy1123@gmail.com>
2020-12-04 13:11:31 +0700

    lesson03-oop 04/12/2020

---
+++ lesson03-oop/src/main/java/Excercises/Color.java
@@ -0,0 +1,5 @@
+package Excercises;
+
+public enum Color {
+	BLACK, WHITE, RED, GOLD, BLUE, GREEN, PINK
+}
---
+++ lesson03-oop/src/main/java/Excercises/Order.java
@@ -0,0 +1,58 @@
+package Excercises;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+public class Order {
+	private Customer customer;
+	private ItemInfo[] itemInfos;
+	private LocalDateTime dateTime;
+	private double totalOfMoney;
+	
+	public Order() {
+	}
+
+	public Order(Customer customer, ItemInfo[] itemInfos, LocalDateTime dateTime) {
+		this.customer = customer;
+		this.itemInfos = itemInfos;
+		this.dateTime = dateTime;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public ItemInfo[] getItemInfos() {
+		return itemInfos;
+	}
+
+	public void setItemInfos(ItemInfo[] itemInfos) {
+		this.itemInfos = itemInfos;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+	
+	public void setTotalOfMoney(double totalOfMoney) {
+		this.totalOfMoney = totalOfMoney;
+	}
+	
+	public double getTotalOfMoney() {
+		return totalOfMoney;
+	}
+	
+	@Override
+	public String toString() {
+		return "Order [customer=" + customer + ", itemInfos=" + Arrays.toString(itemInfos) + ", dateTime="
+				+ dateTime + "]";
+	}
+}
