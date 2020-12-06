33ca9211d3c86da2382af3d8ba6a71c393583fc5
unknown <Dang>
2020-12-06 21:40:38 +0700

    lesson04-oop-features 06-12-2020

---
+++ lesson04-oop-features/src/main/java/inheritance/iinterface/Email.java
@@ -0,0 +1,12 @@
+package inheritance.iinterface;
+
+public interface Email {
+	// class default access modifier => package
+	// interface's default access modifier => project 
+	
+	// interface's method => has no definition
+	
+	
+	boolean login(String username, String password );
+	void logout();
+}
---
+++ lesson04-oop-features/src/main/java/inheritance/iinterface/StandardEmail.java
@@ -0,0 +1,15 @@
+package inheritance.iinterface;
+
+public class StandardEmail implements Email {
+
+	@Override
+	public boolean login(String username, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+  @Override
+public void logout() {
+	// TODO Auto-generated method stub
+	
+}
+}
