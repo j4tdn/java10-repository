3b1cb67f2ed171b285012f292e53d881e73d46e2
khaithmc <khaithumc@gmail.com>
2020-12-06 13:34:26 +0700

    lesson03-dangcongkhaithu 06.12.2020

--- lesson03-dangcongkhaithu/src/main/java/ex03/Main.java
+++ lesson03-dangcongkhaithu/src/main/java/ex03/Main.java
@@ -13,18 +13,25 @@ public class Main {
 		int n = Integer.parseInt(ip.nextLine());
 		int[][] arr = new int[m][n];
 
-		System.out.println("Nhập các phần tử: ");
+		System.out.println("Nhập mảng: ");
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				arr[i][j] = ip.nextInt();
 			}
 		}
 
+		System.out.println("Xuất mảng: ");
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				System.out.println("arr[" + i + "][" + j + "] = " + arr[i][j]);
+				System.out.print(arr[i][j] + "  ");
 			}
+			System.out.println();
 		}
 
+	}
+
+	private static int getYenNguaElement(int[][] arr) {
+
+		return 0;
 	}
 }
