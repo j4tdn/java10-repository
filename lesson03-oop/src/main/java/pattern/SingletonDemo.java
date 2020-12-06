576900a9a6fd94c04300f65008bcb75d2f33ad40
AnhTuan <anhtuannguyen331@gmail.com>
2020-12-06 21:39:51 +0700

    lesson03-oop 06.12.2020

---
+++ lesson03-oop/src/main/java/pattern/SingletonDemo.java
@@ -0,0 +1,14 @@
+package pattern;
+
+public class SingletonDemo {
+	public static void main(String[] args) {
+		SingletonClass s1 = SingletonClass.getInstance();
+		s1.setValue("s1 value");
+		System.out.println("s1: "+ s1.getValue());
+		
+		SingletonClass s2 = SingletonClass.getInstance();
+		System.out.println("s2: "+ s2.getValue());
+		//s2.setValue("s2 value");
+	}
+
+}
