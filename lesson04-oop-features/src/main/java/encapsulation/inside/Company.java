8b3f26577239f900aecfd9267c65d85f8a6d221a
Quyen Phan <Quyen.Phan@mgm-tp.com>
2020-12-11 00:45:05 +0700

    lesson03-oop lesson04-oop-features 11.12.2020

---
+++ lesson04-oop-features/src/main/java/encapsulation/inside/Company.java
@@ -0,0 +1,18 @@
+package encapsulation.inside;
+
+public class Company {
+	String compId;
+	protected int numberOfEmps;
+	private double balance;
+	
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
+}
---
+++ lesson04-oop-features/src/main/java/inheritance/ssuper/role/AbstractEmail.java
@@ -0,0 +1,16 @@
+package inheritance.ssuper.role;
+
+/**
+ * Abstract class contains: JAVA07
+ * 
+ * + normal methods 
+ * + abstract methods: no definition
+ */
+public abstract class AbstractEmail {
+	public abstract boolean login(String username, String password);
+
+	protected boolean login2Steps(String username, String password) {
+		return false;
+	}
+
+}
