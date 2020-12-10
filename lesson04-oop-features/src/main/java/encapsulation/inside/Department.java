7e06d826025cf015576d3e06b7f30ef861d0e469
Vuong Dinh <dinhvanminh617@gmail.com>
2020-12-10 21:46:21 +0700

    lesson04-oop-features 6.12.2020

---
+++ lesson04-oop-features/src/main/java/encapsulation/inside/Department.java
@@ -0,0 +1,11 @@
+package encapsulation.inside;
+
+public class Department {
+	public static void main(String[] args) {
+		Company company = new Company();
+		company.setBalance(1000);
+		company.compID = "X102";
+		company.numberOfEmps = 100;
+	}
+
+}
---
+++ lesson04-oop-features/src/main/java/polymorphism/PoMethod.java
@@ -0,0 +1,39 @@
+package polymorphism;
+
+public class PoMethod {
+	/**
+	 * 
+	 * 2 method in ONE CLASS could be same name when different:
+	 *  + number of parameter
+	 *  + parameter type
+	 *  =>> Overloading
+	 */
+	
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+	
+	public static double sum(double a, double b) {
+		return a + b;
+	}
+	
+	public static int sum(int a, int b, int c) {
+		return a + b + c;
+	}
+	
+	@Override	// Annotation
+	public String toString() {
+		return "PoMethod";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(sum(3, 5));
+		System.out.println(sum(4.5, 5.55));
+		System.out.println(sum(1, 2, 3));
+		
+		PoMethod method = new PoMethod();
+		String text = method.toString();
+		System.out.println(text);
+	}
+
+}
