f65f23e25f639fe7abb2ac9214b6e247d37b1e95
Marcus <vuquang627@gmail.com>
2020-12-13 20:02:37 +0700

    lesson05-vodinhminhquang-13.12.2020

---
+++ lesson05-vodinhminhquang/src/main/java/exam/Exercise01.java
@@ -0,0 +1,18 @@
+package exam;
+
+
+import java.util.ArrayList;
+
+public class Exercise01 {
+	public static void main(String[] args) {
+		int[] arr = {3, 15, 21, 0, 15, 17, 21};
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		System.out.println("=====================");
+
+				
+	}
+}
