f554ccade4aad12cf07d2527d8fbf8552575fd56
tdhung239 <tdhung239@gmail.com>
2020-12-20 21:32:01 +0700

    lesson06-exception 20.12.2020

---
+++ lesson06-exception/src/main/java/exception/Ex01NumberFormatException.java
@@ -0,0 +1,23 @@
+package exception;
+
+import java.util.Scanner;
+
+public class Ex01NumberFormatException {
+	private static Scanner ip = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int number = 0;
+		do {
+			try {
+				System.out.print("Enter a number: ");
+				number = Integer.parseInt(ip.nextLine());
+                break;
+			} catch (NumberFormatException nfe) {
+				System.out.println("Invalid number !");
+			}
+		} while (true);
+
+		System.out.println("Back to main thread");
+
+	}
+}
---
+++ lesson06-exception/src/test/java/Hung/lesson06_exception/AppTest.java
@@ -0,0 +1,20 @@
+package Hung.lesson06_exception;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
