b3f6c613ada47eda3f84e52d2f084e0720682017
dungpro <minamotosakura147258369@gmail.com>
2020-12-20 21:31:33 +0700

    20.12.2020

---
+++ lesson04-oop-features/src/main/java/exercises/Demo.java
@@ -0,0 +1,22 @@
+//package exercises;
+//
+//public class Demo {
+//public static void main(String[] args) {
+//	// Give an ONE direction array integer
+//	// Write method find sum of odd elements
+//	// event 
+//	//power of 2
+//	// prime
+//	int []a = { 1,3,10,8};
+//	System.out.println(sum( a, e-> e %2 ==0));
+//}
+//private static int sum( int []a, Conditon con) {
+//	int sum =0;
+//	for (int element : a) {
+//		if (con.test(element)) {
+//			sum +=element ;
+//		}
+//	}
+//	return sum;
+//}
+//}
---
+++ lesson06-excaption/src/main/java/exception/Ex01NFE.java
@@ -0,0 +1,21 @@
+package exception;
+
+import java.util.Scanner;
+
+public class Ex01NFE {
+	private static Scanner ip =new Scanner(System.in);
+	public static void main(String[] args) {
+		int number =0;
+		do {
+		try {
+			System.out.println(" Enter a number :");
+			 number= Integer.parseInt(ip.nextLine());
+			 break;
+		} catch (NumberFormatException nfe) {
+		System.out.println(" Invalid number !");
+		}
+		}while (true);
+		System.out.println(" back to main thread");
+	}
+
+}
