1bcfd1aa0bd7e83dc4d6d4fbfe20487219589683
nhatTai <nhattaitran1999@gmail.com>
2020-12-24 21:52:57 +0700

    lesson06-nhattai 24.12.2020

---
+++ lesson06-nhattai/src/main/java/ex01/Ex01.java
@@ -0,0 +1,45 @@
+package ex01;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		double result = 0;
+		do {
+			try {
+				result = fde();
+				break;
+			} catch (RuntimeException e) {
+				System.out.println("Nhập lại !!!");
+			}
+		} while (true);
+
+	}
+
+	private static Scanner ip = new Scanner(System.in);
+
+	private static double fde() throws RuntimeException {
+		int a, b;
+		double result = 0;
+		DecimalFormat decimalFormat = new DecimalFormat("#.##"); // làm tròn đến 2 chữ số thập phân
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Nhập vào số a: ");
+		a = scanner.nextInt();
+		System.out.println("Nhập vào số b: ");
+		b = scanner.nextInt();
+
+		if (a == 0) {
+			if (b == 0) {
+				System.out.println("Phương trình này có vô số nghiệm.");
+			} else {
+				throw new RuntimeException();
+			}
+		} else {
+			result = (double) -b / a;
+			System.out.println("Phương trình có nghiệm x = " + decimalFormat.format(result) + ".");
+		}
+		return result;
+	}
+}
---
+++ lesson06-nhattai/src/test/java/nhattai/lesson06_nhattai/AppTest.java
@@ -0,0 +1,20 @@
+package nhattai.lesson06_nhattai;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
