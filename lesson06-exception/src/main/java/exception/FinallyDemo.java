a2488fa9f812d10d356d91686696bd63ba236d64
nhatTai <nhattaitran1999@gmail.com>
2020-12-25 00:21:17 +0700

    lesson06-exception 25.12.2020

---
+++ lesson06-exception/src/main/java/exception/FinallyDemo.java
@@ -0,0 +1,41 @@
+package exception;
+
+import java.awt.Desktop;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class FinallyDemo {
+	public static void main(String[] args) {
+		File file = new File("data.txt");
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		try {
+			fw = new FileWriter(file);
+			bw = new BufferedWriter(fw);
+
+			bw.write("Row x");
+			bw.write("\nRow y");
+
+			bw.write(5 / 0); // exception
+
+			bw.write("\nRow z");
+
+			System.out.println("Finished !!!");
+		} catch (Exception e) {
+
+		} finally {
+			try {
+				bw.close();
+				fw.close();
+
+				Desktop.getDesktop().open(file);
+
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+
+		System.out.println("==== main thread ====");
+	}
+}
