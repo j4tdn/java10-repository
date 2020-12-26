f7766d945fd0bd4645587ed8ffa549f2db40d0d1
phuquoc2109 <46041877+phuquoc2109@users.noreply.github.com>
2020-12-26 12:49:53 +0700

    lesson06-phuquoc 26.12.2020

---
+++ lesson06-phuquoc/src/main/java/ex03/EmailException.java
@@ -0,0 +1,40 @@
+package ex03;
+
+import java.util.Scanner;
+
+import ex02.InvalidCharacter;
+
+public class EmailException {
+	public static Scanner ip = new Scanner(System.in);
+
+	public static void main(String[] args) throws InvalidCharacter {
+		do {
+			System.out.print("Enter A Email : ");
+			String input = ip.nextLine();
+			try {
+				isValid(input);
+				break;
+			} catch (RuntimeException e) {
+				System.out.println(e.getMessage());
+			}
+			System.out.println(" Back to main thread! ");
+		} while (true);
+	}
+
+	private static void isValid(String input) throws InvalidCharacter {
+		if (input.charAt(0) == '@' || input.charAt(input.length() -1) == '@') {
+			throw new InvalidCharacter("The @ character cannot be in the first or last position ");
+		}
+		int count = 0;
+		for (int i = 1; i < input.length() - 1; i++) {
+			if (input.charAt(i) == '@') {
+				count++;
+			}
+		}
+		if (count != 1) {
+			throw new InvalidCharacter("Email address contains only one character @ ");
+		}
+		System.out.println("Email is Valid !");
+	}
+
+}
