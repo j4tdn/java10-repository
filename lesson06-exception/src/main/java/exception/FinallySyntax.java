8dd1b578e622c9dd8d7bc3bf83e7f2c19bd58179
tdhung239 <tdhung239@gmail.com>
2020-12-27 11:14:37 +0700

    lesson06-exception 27.12.2020

---
+++ lesson06-exception/src/main/java/exception/FinallySyntax.java
@@ -0,0 +1,23 @@
+package exception;
+
+public class FinallySyntax {
+	public static void main(String[] args) {
+		// finally = syntax
+		try {
+
+		} finally {
+			// always run
+		}
+		// 1. Open file
+		// 2. write data
+		// 3. save and close
+		try {
+
+		} catch (Exception e) {
+			
+		} finally {
+
+		}
+		System.out.println();
+	}
+}
---
+++ lesson06-exception/src/main/java/exception/InvalidAgeException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class InvalidAgeException extends Exception {
+
+	private static final long serialVersionUID = -8639637553249410321L;
+
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+
+}
