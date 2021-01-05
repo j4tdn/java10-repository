1d4d0661b827bb6dbd542b4e62ebd127fc61a1e9
phuquoc2109 <46041877+phuquoc2109@users.noreply.github.com>
2021-01-05 13:43:17 +0700

    lesson07-phuquoc 05.01.2021

--- lesson07-phuquoc/src/main/java/ex02/SundayCount.java
+++ lesson07-phuquoc/src/main/java/ex02/SundayCount.java
@@ -19,10 +19,13 @@ public class SundayCount {
 		c1.set(Calendar.DAY_OF_MONTH, 1);
 		while (c1.before(c2)) {
 			int da = c1.get(Calendar.DAY_OF_WEEK);
+			int day = c1.get(Calendar.DAY_OF_MONTH);
+			int month = c1.get(Calendar.MONTH);
+			int year = c1.get(Calendar.YEAR);
 			c1.add(Calendar.DAY_OF_MONTH, 1);
 			if (da == Calendar.SUNDAY ) {
 				count++;
-
+				System.out.println(day + "," + month + "," + year +" is sunday");
 			}
 
 		}
