374723dd1e205195321b057103b9e8b068425030
ngthuan <thuannguyen7914@gmail.com>
2021-01-07 17:50:23 +0700

    lesson07-ngocthuan 05.01.2021

---
+++ lesson07-ngocthuan/src/main/java/ex04/Ex04.java
@@ -0,0 +1,33 @@
+package ex04;
+import java.util.Calendar;
+public class Ex04 {
+	public static void main(String[] args) {
+
+		Calendar c = Calendar.getInstance();
+		sundayCount(c);
+	}
+
+	private static void sundayCount(Calendar c) {
+		int count = 0;
+
+		Calendar c1 = Calendar.getInstance();
+
+		Calendar c2 = Calendar.getInstance();
+		c2.set(2021, 01, 01);
+		c1.set(Calendar.DAY_OF_MONTH, 1);
+		while (c1.before(c2)) {
+			int da = c1.get(Calendar.DAY_OF_WEEK);
+			int day = c1.get(Calendar.DAY_OF_MONTH);
+			int month = c1.get(Calendar.MONTH);
+			int year = c1.get(Calendar.YEAR);
+			c1.add(Calendar.DAY_OF_MONTH, 1);
+			if (da == Calendar.SUNDAY ) {
+				count++;
+				System.out.println(day + "," + month + "," + year +" is sunday");
+			}
+
+		}
+		System.out.println("Number of Sundays in the current month: " + count);
+
+	}
+}
---
+++ lesson07-ngocthuan/src/main/java/ex05/Ex05.java
@@ -0,0 +1,19 @@
+package ex05;
+
+public class Ex05 {
+		public static void main(String[] args) {
+			System.out.println(isLeapYear(2020));
+		}
+		 public static boolean isLeapYear(int year) {
+		        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) 
+		        {
+		            System.out.println("This is a leap year!");
+		            return true;
+
+		        }
+		        else {
+		        System.out.println("This is not a leap year!!");
+		        return false;
+		    }
+	}
+}
