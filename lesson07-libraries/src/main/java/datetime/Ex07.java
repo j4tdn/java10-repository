a877273af39b4673c7aacb3a7bed3ceb5db333fb
AnhTuan <anhtuannguyen331@gmail.com>
2021-01-08 00:27:59 +0700

    lesson07-libraries 08.01.2020

---
+++ lesson07-libraries/src/main/java/datetime/Ex07.java
@@ -0,0 +1,42 @@
+package datetime;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+
+public class Ex07 {
+	public static void main(String[] args) {
+		Calendar start = Calendar.getInstance();
+		start.set(2021, Calendar.JANUARY, 6, 0, 0, 0);
+		
+		Calendar today = Calendar.getInstance();
+		
+		if(today.before(start)) {
+			throw new RuntimeException("start date cannot greater than today!!");
+			
+		}
+		
+		long lstrat = start.getTimeInMillis();
+		long lend = today.getTimeInMillis();
+		long duration = lend-lstrat;
+		long left = duration;
+		
+		long days = TimeUnit.MILLISECONDS.toDays(duration);
+		duration -= TimeUnit.DAYS.toMillis(days);
+		
+		long hours = TimeUnit.MILLISECONDS.toHours(duration);
+		duration -= TimeUnit.HOURS.toMillis(hours);
+
+		long minutes = TimeUnit.MILLISECONDS.toMinutes(duration);
+		duration -= TimeUnit.HOURS.toMillis(minutes);
+		
+		long seconds = TimeUnit.MILLISECONDS.toSeconds(duration);
+		duration -= TimeUnit.HOURS.toMillis(seconds);
+		
+		System.out.println("day" + days);
+		System.out.println("hour" + hours);
+		System.out.println("minute" + minutes);
+		System.out.println("second" + seconds);
+	}
+
+}
---
+++ lesson07-libraries/src/main/java/utils/DateUtils.java
@@ -0,0 +1,84 @@
+package utils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Objects;
+
+public class DateUtils {
+	private DateUtils() {
+
+	}
+
+	/**
+	 * Convert calendar to date
+	 */
+
+	public static Date toDate(Calendar c) {
+		Objects.requireNonNull(c, "calendar cannt be null.");
+		return c.getTime();
+	}
+
+	/**
+	 * 
+	 * Convert string to date Formated: đ.MM.yyyy
+	 */
+
+	public static Date toDate(String s) {
+		Objects.requireNonNull(s, "text date cannt be null.");
+		DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+		Date date = null;
+		try {
+			date = df.parse(s);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return date;
+	}
+	
+	/**
+	 * Convert date to calendar
+	 */
+
+	public static Calendar toCalendar(Date date) {
+		Objects.requireNonNull(date, "date cannot be null.");
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		return c;
+	}
+	
+	/**
+	 * 
+	 * Convert string to calendar
+	 */
+	public static Calendar toCalendar(String s) {
+		Objects.requireNonNull(s, "date cannot be null.");
+		return toCalendar(toDate(s));
+	}
+
+
+	/**
+	 * Convert date to string // 07-Jan-21 => 07.01.2021 | 07/01/2021 || 2021-01-07
+	 */
+
+	public static String toString(Date date) {
+		Objects.requireNonNull(date, "date cannt be null.");
+		// Thu Jan 07 23:05:35 ICT 2021
+		DateFormat df = new SimpleDateFormat("EEE LLL dd HH:mm:ss z yyyy");
+		return df.format(date);
+	}
+
+	public static String toString(Calendar c) {
+		Objects.requireNonNull(c, "calendar cannt be null.");
+		return toString(c.getTime());
+	}
+
+	public static boolean isLeapTear(int year) {
+		GregorianCalendar g = new GregorianCalendar();
+		return g.isLeapYear(year);
+	}
+}
