2d0108897e72ecdaba26bafcfe2921ebbdfa8ddc
huuphuc <phuctrana3k58@gmail.com>
2021-01-08 07:14:38 +0700

    lesson07-libraries 08.01.2021

---
+++ lesson07-libraries/src/main/java/datetime/Ex05.java
@@ -0,0 +1,10 @@
+package datetime;
+
+import utils.DateUtils;
+
+public class Ex05 {
+	public static void main(String[] args) {
+		int year = 200;
+		System.out.println("isLeapYear: " + DateUtils.isLeapYear(year));
+	}
+}
