5c508ee1bc6d50dd9f19cd043dc5eed2aa175ae8
unknown <Dang>
2021-01-10 21:23:54 +0700

    lesson07-libraries lesson08-string 10.01.2021

---
+++ lesson07-libraries/src/main/java/localdatatime/Ex03.java
@@ -0,0 +1,17 @@
+package localdatatime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Ex03 {
+public static void main(String[] args) {
+	LocalDateTime ldt = LocalDateTime.now();
+	LocalDate ldate = ldt.toLocalDate();
+	LocalTime ltime = ldt.toLocalTime();
+	
+	ldt = ldate.atTime(ltime);
+	ldt = ltime.atDate(ldate);
+	
+}
+}
---
+++ lesson08-string/src/main/java/imutable/Ex02.java
@@ -0,0 +1,32 @@
+package imutable;
+
+public class Ex02 {
+public static void main(String[] args) {
+	// string literal ==> HEAP ==> CONSTANT POOL
+	
+	String l1 = "Hello";
+	String l2 = "Hello";
+	String l3 = "Student";
+	
+	hash("l1",l1);
+	hash("l2",l2);
+	hash("l3",l3);
+	
+	l1 = "Welcome";
+	l1 = "Student";
+	value("l1",l1);
+	hash("l1",l1);
+}
+	
+
+	private static void hash(String name, String s) {
+		System.out.println(name + ": " + System.identityHashCode(s));
+
+	}
+
+	private static void value(String name, String s) {
+		System.out.println(name + ": " + s );
+	}
+	
+}
+
