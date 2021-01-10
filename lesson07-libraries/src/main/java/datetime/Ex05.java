e8dea2ed945661918cd9345cc89bcb25af389048
thenguyen <0984894089n@gmail.com>
2021-01-10 13:40:45 +0700

    lesson07-libraries 10.1.2021

---
+++ lesson07-libraries/src/main/java/datetime/Ex05.java
@@ -0,0 +1,10 @@
+package datetime;
+
+import utils.DateUtils;
+
+public class Ex05 {
+public static void main(String[] args) {
+	int year = 2000;
+	System.out.println("isLeapYear: " + DateUtils.isLeapTear(year));
+}
+}
