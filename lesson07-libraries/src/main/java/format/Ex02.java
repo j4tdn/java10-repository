6367664843690f634e5050bdd0c0d1fd93f1a566
taquocthang <tathang0330@gmail.com>
2021-01-10 21:15:14 +0700

    lesson07-libraries 10.01.2021

---
+++ lesson07-libraries/src/main/java/format/Ex02.java
@@ -0,0 +1,16 @@
+package format;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+
+
+public class Ex02 {
+	public static void main(String[] args) {
+		double number = 456123456.7268d;
+		DecimalFormat df = new DecimalFormat("#,###.000");
+		String result = df.format(number);
+		System.out.println("result: " + result);
+		
+		//System.out.println(Locale.getDefault());
+	}
+}
---
+++ lesson07-libraries/src/main/java/localdatetime/Ex02.java
@@ -0,0 +1,22 @@
+package localdatetime;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		LocalTime ltStart = LocalTime.of(10, 10, 10);
+		LocalTime ltEnd = LocalTime.of(12, 11, 30);
+		
+		System.out.println("ltime: " + ltStart);
+		System.out.println("hour: " + ltStart.getHour());
+		System.out.println("minutes: " + ltStart.getMinute());
+		System.out.println("second: " + ltStart.getSecond());
+		
+		Duration duration = Duration.between(ltStart, ltEnd);
+		String dString = duration.toHoursPart()+ " H " 
+					 + duration.toMinutesPart()+ " M "
+					 + duration.toSecondsPart()+ " S ";
+		System.out.println("duration: " +dString);
+	}
+}
