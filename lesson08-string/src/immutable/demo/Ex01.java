eb2f8e6a3f17a6f2d317c580ee071c00a8d66287
Marcus <vuquang627@gmail.com>
2021-01-15 02:16:03 +0700

    lesson08-string 15.01.2021

---
+++ lesson08-string/src/immutable/demo/Ex01.java
@@ -0,0 +1,20 @@
+package immutable.demo;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		String s = "Welcome";
+		System.out.println(s.length());
+		
+		String s1 = s.concat(" to Java10");
+		System.out.println(s1);
+		
+		System.out.println(s.charAt(3));
+		
+		for (int i = 0; i < s.length(); i++) {
+			System.out.println(s.charAt(i) + " ");
+		}
+		
+		System.out.println(s.indexOf('e'));
+		System.out.println(s.lastIndexOf('e'));
+	}
+}
