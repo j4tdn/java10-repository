f59389cfb69d04e8d2b1461e724d881e526d0608
tdhung239 <tdhung239@gmail.com>
2021-01-16 16:17:02 +0700

    lesson08-string 16.01.2021

---
+++ lesson08-string/src/main/java/immutable/demo/Ex04.java
@@ -0,0 +1,20 @@
+package immutable.demo;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		// String literal
+		String s1 = "ab";
+		String s2 = "ab";
+		String s3 = "AB";
+
+		System.out.println("==: " + (s1 == s2));
+		System.out.println("s1 equals s2: " + (s1.equals(s2)));
+		System.out.println("s1 equals s3: " + (s1.equals(s3)));
+
+		System.out.println("s1 equalsIgnoreCase: " + (s1.equalsIgnoreCase(s2)));
+
+		System.out.println("S1 compareTo (s2): " + s1.compareTo(s2));
+		System.out.println("S1 compareTo (s2): " + s1.compareTo(s3));
+
+	}
+}
