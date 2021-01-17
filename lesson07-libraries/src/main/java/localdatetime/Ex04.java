91f9af493ba9bb98fcaa191cff70b32d539c35ae
phuquoc2109 <46041877+phuquoc2109@users.noreply.github.com>
2021-01-17 16:01:09 +0700

    lesson07-libraries 17.01.2021

---
+++ lesson07-libraries/src/main/java/localdatetime/Ex04.java
@@ -0,0 +1,36 @@
+package localdatetime;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		LocalDateTime ldtStart = LocalDateTime.of(2021, 10, 8, 10, 20, 10);
+		LocalDateTime ldtEnd = LocalDateTime.of(2021, 10, 10, 8, 22, 20);
+		
+		String result = between(ldtStart, ldtEnd);
+		System.out.println(result );
+	}
+	
+	private static String between(LocalDateTime ldtStart, LocalDateTime ldtEnd) {
+		LocalDate ldStart = ldtStart.toLocalDate();
+		LocalDate ldEnd = ldtEnd.toLocalDate();
+		
+		LocalTime ltStart = ldtStart.toLocalTime();
+		LocalTime ltEnd = ldtEnd.toLocalTime();
+		
+		Period period = Period.between(ldStart, ldEnd);
+		Duration duration = Duration.between(ltStart, ltEnd);
+		
+		if(ltEnd.isBefore(ltStart)) {
+			duration = duration.plusDays(1);
+			period = period.minusDays(1);
+		}
+		
+		return period.toString() + "'T'" + duration.toString();
+		
+	}
+}
