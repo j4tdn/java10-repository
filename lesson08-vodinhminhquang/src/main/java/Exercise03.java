52fc80d7b64e786bce2c7e35a7159b68dda52718
Marcus <vuquang627@gmail.com>
2021-01-18 18:25:36 +0700

    lesson08-vodinhminhquang-18.01.2021

---
+++ lesson08-vodinhminhquang/src/main/java/Exercise03.java
@@ -0,0 +1,11 @@
+
+import java.text.Normalizer;
+
+public class Exercise03 {
+	public static void main(String[] args) {
+		String string = "Em có yêu anh không anh để anh biết còn chờ";
+		string = Normalizer.normalize(string, Normalizer.Form.NFD);
+		string = string.replaceAll("\\p{M}", "");
+		System.out.println(string);
+	}
+} 
