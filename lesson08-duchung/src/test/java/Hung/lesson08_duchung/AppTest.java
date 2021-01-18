e527ccc13f9542da20491b1c1dc1201caa310a34
tdhung239 <tdhung239@gmail.com>
2021-01-18 15:51:28 +0700

    lesson08-duchung 18.01.2021

---
+++ lesson08-duchung/src/test/java/Hung/lesson08_duchung/AppTest.java
@@ -0,0 +1,20 @@
+package Hung.lesson08_duchung;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
