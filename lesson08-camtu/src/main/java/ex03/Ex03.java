6980dd6302c221ff78cf36bc5e8f1f5c4718bd81
camtu <camtudanang@gmail.com>
2021-01-19 22:37:25 +0700

    lesson08-camtu 18.01.21

---
+++ lesson08-camtu/src/main/java/ex03/Ex03.java
@@ -0,0 +1,18 @@
+package ex03;
+
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		String input = "Em có yêu anh không anh để anh biết còn chờ \n"
+				+ "Em xin lỗi em đã có thái độ không đúng với anh";
+		System.out.println(removeAccent(input));
+	}
+	
+	private static String removeAccent(String s) {
+		String temp =  Normalizer.normalize(s, Normalizer.Form.NFD);
+		Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+		return pattern.matcher(temp).replaceAll("");
+	}
+}
