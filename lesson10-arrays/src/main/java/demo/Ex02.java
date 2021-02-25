3c6d9d3e751f83d2960f6025731402d6313fe926
AnhTuan <anhtuannguyen331@gmail.com>
2021-02-25 10:20:38 +0700

    lesson10-arrays 25.02.2021

---
+++ lesson10-arrays/src/main/java/demo/Ex02.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class Ex02 {
+	public static void main(String[] args) {
+
+		int[] number = { 1, 2, 3, 4, 5 };
+		Reverse(number);
+
+	}
+
+	private static void Reverse(int[] number) {
+		for (int i = 0; i < number.length; i++) {
+			for (int j = i + 1; j < number.length; j++) {
+					int temp = number[j]; 
+					number[j] = number[i];
+					number[i] = temp;
+			}
+			System.out.print(number[i] + " ");
+		}
+	}
+}
