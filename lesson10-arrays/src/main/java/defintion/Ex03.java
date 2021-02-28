ccb5cb30de081fa6cb2858f5beadca1dd1603e7a
anhphuc013567 <anhphuc013567@gmail.com>
2021-02-28 21:31:22 +0700

    lesson10-arrays 28.02.2021

---
+++ lesson10-arrays/src/main/java/defintion/Ex03.java
@@ -0,0 +1,19 @@
+package defintion;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		int[] digits = { 1, 6 };
+		
+		doSomething(digits);
+		forEach(digits);
+	}
+
+	private static void forEach(int[] digits) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void doSomething(int[] items) {
+		items[0] = 20;
+	}
+}
---
+++ lesson10-arrays/src/main/java/defintion/Ex05.java
@@ -0,0 +1,18 @@
+package defintion;
+
+public class Ex05 {
+	public static void main(String[] args) {
+		// 1 dimensions
+		int[] a = { 1, 2, 3 };
+
+		// 2 dimensions
+		// rows:3, columns:2
+		int[][] b = { { 1, 2 }, { 4, 5,6 }, { 7, 8,9 } };
+		forIndex2D(b);
+	}
+
+	private static void forIndex2D(int[][] b) {
+		// TODO Auto-generated method stub
+		
+	}
+}
