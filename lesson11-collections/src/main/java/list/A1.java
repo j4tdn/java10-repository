7f8b37459a180ab3b083d3e05141ce6ad6083e11
phuquoc <quocduongphu@gmail.com>
2021-03-03 21:31:50 +0700

    lesson11-collections 03.03.2021

---
+++ lesson11-collections/src/main/java/list/A1.java
@@ -0,0 +1,27 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A1 {
+	public static void main(String[] args) {
+		// before 1.5 : raw type
+		List list = new ArrayList(); // objects
+		list.add(1);
+		list.add("a");
+		list.add(2d);
+		for(Object object: list) {
+			System.out.println((Integer)object * 2);
+		}
+		
+		// after 1.5 : generic type
+		List<Integer> digits = new ArrayList<>();
+		digits.add(1);
+		// digits.add("b");
+		for(Integer digit: digits) {
+			System.out.println(digit*2);
+		}
+	}
+	
+	// note: generic type supports to catching error at compile time
+}
