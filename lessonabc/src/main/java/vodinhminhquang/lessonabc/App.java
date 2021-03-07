3885288f3f3ab00e7f31bf5f608c3bd76d93a8eb
Marcus <vuquang627@gmail.com>
2021-03-07 18:05:26 +0700

    Update

---
+++ lessonabc/src/main/java/vodinhminhquang/lessonabc/App.java
@@ -0,0 +1,13 @@
+package vodinhminhquang.lessonabc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
