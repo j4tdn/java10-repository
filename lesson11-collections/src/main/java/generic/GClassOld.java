76cb935f69378556335622465cfe7882993716b1
tdhung239 <tdhung239@gmail.com>
2021-03-10 21:56:16 +0700

    lesson11-collections 10.03.2021

---
+++ lesson11-collections/src/main/java/generic/GClassOld.java
@@ -0,0 +1,26 @@
+package generic;
+
+public class GClassOld {
+	private Object[] objects;
+
+	public void set(Object[] objects) {
+		this.objects = objects;
+	}
+
+	public Object[] get() {
+		return objects;
+	}
+	
+	public boolean add (Object object) {
+		objects[0]=object;
+		return true;
+	}
+	// fail at runtime, not fail at compiled
+	public static void main(String[] args) {
+		GClassOld gclass= new GClassOld();
+		gclass.set(new Integer[] {1,2,3});
+		gclass.add("xxx");
+		
+	}
+
+}
