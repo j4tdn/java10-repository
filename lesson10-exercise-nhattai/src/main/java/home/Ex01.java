55925df1141968b16de26283a2d12fc4cd17b212
nhatTai <nhattaitran1999@gmail.com>
2021-03-18 10:19:02 +0700

    lesson10-nhattai-update 18.03.2021

---
+++ lesson10-exercise-nhattai/src/main/java/home/Ex01.java
@@ -0,0 +1,96 @@
+package home;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		int[] digits = { 1, 2, 3, 4, 3, 1 };
+		System.out.println(Arrays.toString(distinct(digits).toArray()));
+
+		System.out.println("=====================");
+
+		int[] numbers = { 1, 9, 5, 6, 4, 8, 3 };
+		insertionSort(numbers);
+
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.print(numbers[i] + " ");
+		}
+		System.out.println();
+		System.out.println("======================");
+
+		int max = numbers[numbers.length - 1];
+		int count = 0;
+		for (int i = numbers.length - 1; i >= 0; i--) {
+			if (numbers[i] < max) {
+				count++;
+				max = numbers[i];
+				if (count == 2) {
+					System.out.println("third Max: " + numbers[i]);
+					break;
+				}
+			}
+		}
+
+		System.out.println();
+		System.out.println("======================");
+
+		List<Integer> sortedList = new ArrayList<>();
+		for (int number : numbers) {
+			sortedList.add(number);
+		}
+
+		Set<Integer> set = new LinkedHashSet<>(sortedList);
+		for (Integer item : set) {
+			System.out.print(item + " ");
+		}
+		System.out.println();
+
+		sortedList = new ArrayList<>(set);
+		System.out.println("third Max cach 2: " + sortedList.get(sortedList.size() - 3));
+	}
+
+	private static List<Integer> distinct(int[] digits) {
+		List<Integer> result = new ArrayList<>();
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int digit : digits) {
+			if (!map.containsKey(digit)) {
+				map.put(digit, 1);
+			} else {
+				int value = map.get(digit);
+				map.put(digit, value + 1);
+			}
+		}
+
+		for (Entry<Integer, Integer> entry : map.entrySet()) {
+			if (entry.getValue() == 1) {
+				result.add(entry.getKey());
+			}
+		}
+		return result;
+	}
+
+	private static void insertionSort(int[] digits) {
+		int posToInsert;
+		int valToInsert;
+		for (int i = 0; i < digits.length; i++) {
+			valToInsert = digits[i];
+			posToInsert = i;
+			// shift, get position to insert
+			while (posToInsert > 0 && digits[posToInsert - 1] > valToInsert) {
+				digits[posToInsert] = digits[posToInsert - 1];
+				posToInsert--;
+			}
+			if (posToInsert != i) {
+				digits[posToInsert] = valToInsert;
+			}
+		}
+	}
+
+}
---
+++ lesson10-exercise-nhattai/src/test/java/nhattai/lesson10_exercise_nhattai/AppTest.java
@@ -0,0 +1,20 @@
+package nhattai.lesson10_exercise_nhattai;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
