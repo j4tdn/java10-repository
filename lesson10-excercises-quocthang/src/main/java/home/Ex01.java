afb0dbae0e5b1203423c4f6000c8193ead9b68f1
taquocthang <tathang0330@gmail.com>
2021-03-18 10:13:25 +0700

    lesson10-excercises-quocthang 18.03.2021

---
+++ lesson10-excercises-quocthang/src/main/java/home/Ex01.java
@@ -0,0 +1,91 @@
+package home;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		int[] digits = { 1, 2, 3, 4, 3, 1, 9, 7, 8 };
+		System.out.println(Arrays.toString(distinct(digits).toArray()));
+
+		System.out.println("========================");
+		int[] numbers = { 1, 10, 9, 12, 8, 4, 12 };
+		insertionSort(numbers);
+
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.print(numbers[i] + " ");
+		}
+
+		System.out.println("\n========================");
+		int max = numbers[numbers.length - 1];
+		int count = 0;
+		for (int i = numbers.length - 1; i >= 0; i--) {
+			if (numbers[i] < max) {
+				count++;
+				max = numbers[i];
+				if (count == 2) {
+					System.out.println("third max: " + numbers[i]);
+					break;
+				}
+			}
+		}
+		
+		// int[] digits => List<Integer>
+		List<Integer> sortedList = new ArrayList<>();
+		for(int number: numbers) {
+			sortedList.add(number);
+		}
+		Set<Integer> set = new LinkedHashSet<>(sortedList);
+		for(Integer item: set) {
+			System.out.print(item + " ");
+		}
+		sortedList = new ArrayList<>(set);
+		System.out.print("\nthird max: " + sortedList.get(sortedList.size() - 3));
+
+	}
+
+	private static void insertionSort(int[] digits) {
+		int valToInsert;
+		int posToInsert;
+		for (int i = 0; i < digits.length; i++) {
+			valToInsert = digits[i];
+			posToInsert = i;
+
+			// shift, get position to insert
+			while (posToInsert > 0 && digits[posToInsert - 1] > valToInsert) { // increase
+				digits[posToInsert] = digits[posToInsert - 1];
+				posToInsert--;
+			}
+			if (posToInsert != i) {
+				digits[posToInsert] = valToInsert;
+
+			}
+		}
+	}
+
+	private static List<Integer> distinct(int[] digits) {
+		List<Integer> result = new ArrayList<>();
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int digit : digits) {
+			if (!map.containsKey(digit)) {
+				map.put(digit, 1);
+			} else {
+				int value = map.get(digit);
+				map.put(digit, value + 1);
+			}
+		}
+
+		for (Entry<Integer, Integer> entry : map.entrySet()) {
+			if (entry.getValue() == 1)
+				result.add(entry.getKey());
+		}
+		return result;
+	}
+}
---
+++ lesson10-excercises-quocthang/src/main/java/home/Ex03.java
@@ -0,0 +1,47 @@
+package home;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		String[] strings = { "-2", "-6", "10", "12", null, "4", "8", null, "Special", "a", "c", "b", "xx" };
+		print(strings);
+		Arrays.sort(strings, new Comparator<String>() {
+
+			@Override
+			public int compare(String s1, String s2) {
+				if (s1 == null) {
+					return 1;
+				}
+				if (s2 == null) {
+					return -1;
+				}
+				if (s1.equals("Special")) {
+					return -1;
+				}
+				if (s2.equals("Special")) {
+					return 1;
+				}
+				
+				// number or negative number
+				if(s1.matches("-?[0-9]+")) {
+					Integer i1 = Integer.parseInt(s1);
+					if(s2.matches("-?[0-9]+")) {
+						Integer i2 = Integer.parseInt(s2);
+						return i1.compareTo(i2);
+					}
+				}
+				return s1.compareTo(s2);
+			}
+		});
+		print(strings);
+	}
+
+	private static void print(String[] strings) {
+		System.out.println();
+		for (String str : strings) {
+			System.out.print(str + " ");
+		}
+	}
+}
