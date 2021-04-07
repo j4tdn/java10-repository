c720e0be05d35fcb540afbfa1f758dda088b12cf
Marcus <vuquang627@gmail.com>
2021-04-07 21:30:45 +0700

    lesson12-stream 07.04.2020

--- lesson12-stream/src/main/java/main/App.java
+++ lesson12-stream/src/main/java/main/App.java
@@ -1,14 +1,19 @@
-package bean;
+package main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+
+import bean.Apple;
 
 public class App {
 	public static void main(String[] args) {
@@ -27,7 +32,6 @@ public class App {
 						(o1, o2) -> o2, //merge function --> hai objects giong nhau thi lay cai nao
 						() -> new LinkedHashMap<>())); 	// map supplier
 		// case 2: with method reference and constructor reference
-		
 		  Map<Integer, Apple> map02 = inventory.stream()
 		  .collect(Collectors.toMap(Apple::getId, // không trả về một interger, trả về một instance của functional interface một abstract class(.apply())
 				  Function.identity(),
@@ -54,16 +58,48 @@ public class App {
 		Apple a1 = sup.get();
 		System.out.println(a1);
 		// interfere
-		Function<String, Apple> sup2 = (String t) -> new Apple(t);
+		Function<String, Apple> sup2 = Apple::new;
 		Apple a2 = sup2.apply("green");
 		System.out.println(a2);
 		// 2 parameters
-		BiFunction<String, String, Apple> biFunc = (String t1, String t2) -> new Apple(t1, t2);
+		BiFunction<String, String, Apple> biFunc = Apple::new;
 		Apple a3 = biFunc.apply("green", "Vietnam");
 		System.out.println(a3);
 		// 3 parameters
+		System.out.println("=======================");
+		
+		Comparator<Apple> c1 = (ap1, ap2) -> ap1.getId() - ap2.getId();
+		Comparator<Apple> c2 = (ap1, ap2) -> Double.compare(ap1.getWeight(), ap2.getWeight());
+		
+		inventory.sort(c1.thenComparing(c2));
+		inventory.forEach(System.out::println);
+		
+		System.out.println("=======================");
+		
+		Predicate<Apple> redApplePre = a -> "red".equals(a.getColor());
+		Predicate<Apple> weightApplePre = a -> a.getWeight() > 400;
+		
+		filter(inventory, redApplePre.and(weightApplePre)).forEach(System.out::println);
+		
+		System.out.println("=======================");
+		Function<Integer, Integer> f = x -> x + 2;
+		Function<Integer, Integer> g = x -> x * 3;
+		Function<Integer, Integer> r = f.compose(g);
+		
+		System.out.println("digit: " + r.apply(3));
+		
 	}
-
+	private static List<Apple> filter(List<Apple> inventory, Predicate<Apple> predicate){
+		List<Apple> result = new ArrayList<>();
+		for(Apple apple : inventory) {
+			if(predicate.test(apple)) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
+	
+	
 	public static List<Apple> getAll() {
 		return Arrays.asList(new Apple(11, "green", 200, "Vietnam"), new Apple(2, "green", 300, "Vietnam"),
 				new Apple(9, "red", 300, "Lao"), new Apple(4, "yellow", 400, "Thailand"),
