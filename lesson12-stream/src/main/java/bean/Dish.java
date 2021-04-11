5453b867d7bd3c3990eea5c4bdef70b97b9c6130
tdhung239 <tdhung239@gmail.com>
2021-04-11 20:30:06 +0700

    lesson12-stream 11.04.2021

--- lesson12-stream/src/main/java/bean/Dish.java
+++ lesson12-stream/src/main/java/bean/Dish.java
@@ -1,57 +1,89 @@
 package bean;
 
+import java.util.Objects;
+
 public class Dish {
-    private String id;
-    private String name;
-    private double calories;
-    private boolean vegetarian;
+	public enum Kind {
+		MEAT, FISH, OTHER
+	}
 
+	private String id;
+	private String name;
+	private double calories;
+	private Kind kind;
+	private boolean vegetarian;
 
-    public Dish() {
-    }
+	public Dish() {
+	}
 
-    public Dish(String id, String name, double calories, boolean vegetarian) {
-        this.id = id;
-        this.name = name;
-        this.calories = calories;
-        this.vegetarian = vegetarian;
+	public Dish(String id, String name, double calories, Kind kind, boolean vegetarian) {
+		this.id = id;
+		this.name = name;
+		this.calories = calories;
+		this.kind = kind;
+		this.vegetarian = vegetarian;
 
-    }
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public double getCalories() {
-        return calories;
-    }
+	public double getCalories() {
+		return calories;
+	}
 
-    public void setCalories(double calories) {
-        this.calories = calories;
-    }
+	public void setCalories(double calories) {
+		this.calories = calories;
+	}
 
-    public boolean isVegetarian() {
-        return vegetarian;
-    }
+	public boolean isVegetarian() {
+		return vegetarian;
+	}
 
-    public void setVegetarian(boolean vegetarian) {
-        this.vegetarian = vegetarian;
-    }
+	public void setVegetarian(boolean vegetarian) {
+		this.vegetarian = vegetarian;
+	}
 
-    @Override
-    public String toString() {
-        return name;
-    }
+	public void setKind(Kind kind) {
+		this.kind = kind;
+	}
+
+	public Kind getKind() {
+		return kind;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(getCalories());
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this==o) {
+			return true;
+		}
+		if(!(o instanceof Dish)) {
+			return false;
+		}
+		Dish that = (Dish)o;
+		return Double.compare(getCalories(), that.getCalories()) == 0;
+	}
+
+	@Override
+	public String toString() {
+		return id + "," + name + "," + calories + "," + kind + "," + vegetarian;
+	}
 }
--- lesson12-stream/src/main/java/main/StreamApp.java
+++ lesson12-stream/src/main/java/main/StreamApp.java
@@ -1,11 +1,15 @@
 package main;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import bean.Dish;
-
+import bean.Dish.Kind;
 public class StreamApp {
 	public static void main(String[] args) {
 		// data source
@@ -22,16 +26,53 @@ public class StreamApp {
 				.collect(Collectors.toList()); // List <String>
 		         result.forEach(System.out::println);
 		
-		//print traversable
+		// stream print traversable
+		         System.out.println("=========================");
+		// intermediate operation versus terminal operation
+		// 
+		        long count = menu.stream()
+		         	.filter(d -> {
+		         		System.out.println("filter " +d.getName());
+		         		return d.getCalories() > 300;
+		         	}).map(d -> {
+		         		System.out.println("map " + d.getName());
+		         		return d.getName();
+		         	})
+		         	.limit(2)
+		         	.count();
+		        System.out.println("count: " +count);
+		        
+		// 
+		        
+		        System.out.println("===================");
+		        List<Dish> meatDished =  menu.stream()
+		        	 .filter(d->Kind.MEAT.equals(d.getKind()))
+		        	//.distinct() // hashcode, equals according to property which produce 
+		        	// cach 1 su dung distinct 
+		        	.filter(distinctBy(Dish::getCalories))
+		        	// cach 2 overide function distinct
+		        	.collect(Collectors.toList());
+		      
+		      meatDished.forEach(System.out::println);
+		        
+		
+	}
+	
+	private static <T, R> Predicate<T> distinctBy( Function<T, R> func) {
+		Set<R> noDup = new HashSet<>();
+		return t -> noDup.add(func.apply(t)); 
 	}
 
 	private static List<Dish> getAll() {
 		return Arrays.asList(
-				new Dish("1", "pork", 308, false), 
-				new Dish("2", "beef", 105, true),
-				new Dish("3", "chicken", 329, true), 
-				new Dish("4", "dog", 302, false),
-				new Dish("5", "meomeo", 480, true));
+				new Dish("1", "D1",620,Kind.MEAT, false), 
+				new Dish("2", "D2",620,Kind.OTHER, false), 
+				new Dish("3", "D3", 329,Kind.OTHER, true),
+				new Dish("4", "D4", 302,Kind.MEAT, true), 
+				new Dish("5", "D5", 480,Kind.MEAT, false),
+				new Dish("6", "D6", 480,Kind.MEAT, true),
+				new Dish("7", "D7", 480,Kind.MEAT, true))
+				;
 	}
 
 }
