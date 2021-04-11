294ae55be8630a74c081d433d161ab8c3ffde231
AnhTuan <anhtuannguyen331@gmail.com>
2021-04-11 06:29:12 -0700

    lesson12-stream 11.04.2021

--- lesson12-stream/src/main/java/main/StreamApp.java
+++ lesson12-stream/src/main/java/main/StreamApp.java
@@ -2,10 +2,15 @@ package main;
 
 import java.awt.dnd.DropTargetAdapter;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import bean.Dish;
+import bean.Dish.Kind;
 
 public class StreamApp {
 
@@ -23,12 +28,54 @@ public class StreamApp {
 				.collect(Collectors.toList());// List<String>
 
 		result.forEach(System.out::println);
+
+		System.out.println("====================");
+		// intermediate versus terminal operation
+
+		long count = menu.stream().filter(d -> {
+			System.out.println("filter " + d.getName());
+			return d.getCalories() > 300;
+		})// Stream<Dish>
+				.map(d -> {
+					System.out.println("map " + d.getName());
+					return d.getName();
+				})// Stream<String>
+				.limit(2).count();
+
+		System.out.println("count: " + count);
+		
+		// get MEAT dishes from initial menu
+		// and makes sure that there are no
+		// duplicates of calories
+		
+		// List<Dish>
+		
+		System.out.println("===================");
+		
+		List<Dish> meatDishes = menu.stream()
+			.filter(d -> Kind.MEAT.equals(d.getKind()))
+			//.distinct()
+			// hash code, equals according to
+			// property which produce non-duplicate
+			// results
+			.filter(distinctBy(Dish::getCalories))
+			.collect(Collectors.toList());
+		
+		meatDishes.forEach(System.out::println);
+	}
+	private static <T, R> Predicate<Dish> distinctBy(Function<Dish, R> func){
+		Set<R> noDup = new HashSet<>();
+		return t -> noDup.add(func.apply(t));
 	}
 
 	private static List<Dish> getAll() {
-		return Arrays.asList(new Dish("1", "pork", 308, false), new Dish("2", "beef", 105, true),
-				new Dish("3", "chicken", 329, true), new Dish("4", "dog", 302, false),
-				new Dish("5", "meomeo", 480, true));
+		return Arrays.asList(new Dish("1", "D1", 308, Kind.MEAT, false), 
+				new Dish("2", "D2", 301, Kind.OTHER, true),
+				new Dish("3", "D3", 329, Kind.OTHER, true), 
+				new Dish("4", "D4", 302, Kind.MEAT, false),
+				new Dish("5", "D5", 480, Kind.MEAT, false), 
+				new Dish("6", "D6", 480, Kind.MEAT, false),
+				new Dish("7", "D7", 818, Kind.MEAT, false));
 
 	}
 }
