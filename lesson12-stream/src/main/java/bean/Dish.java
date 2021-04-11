e59a41a884a649fe8d1305e932da5bb1e2722bfc
duonghuy1123 <duoonghuy1123@gmail.com>
2021-04-11 20:30:37 +0700

    lesson12-stream 11/04/2021

--- lesson12-stream/src/main/java/bean/Dish.java
+++ lesson12-stream/src/main/java/bean/Dish.java
@@ -1,55 +1,91 @@
 package bean;
 
+import java.util.Objects;
+
 public class Dish {
+	
+	public enum Kind {
+		MEAT, FISH, OTHER
+	};
+	
     private String id;
     private String name;
     private double calories;
+    private Kind kind;
     private boolean vegetarian;
 
     public Dish() {
     }
 
-    public Dish(String id, String name, double calories, boolean vegetarian) {
-        this.id = id;
-        this.name = name;
-        this.calories = calories;
-        this.vegetarian = vegetarian;
-    }
+    public Dish(String id, String name, double calories, Kind kind, boolean vegetarian) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.calories = calories;
+		this.kind = kind;
+		this.vegetarian = vegetarian;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public double getCalories() {
-        return calories;
-    }
+	public double getCalories() {
+		return calories;
+	}
 
-    public void setCalories(double calories) {
-        this.calories = calories;
-    }
+	public void setCalories(double calories) {
+		this.calories = calories;
+	}
 
-    public boolean isVegetarian() {
-        return vegetarian;
-    }
+	public Kind getKind() {
+		return kind;
+	}
 
-    public void setVegetarian(boolean vegetarian) {
-        this.vegetarian = vegetarian;
-    }
+	public void setKind(Kind kind) {
+		this.kind = kind;
+	}
 
-    @Override
-    public String toString() {
-        return name;
-    }
+	public boolean isVegetarian() {
+		return vegetarian;
+	}
+
+	public void setVegetarian(boolean vegetarian) {
+		this.vegetarian = vegetarian;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Dish)) {
+			return false;
+		}
+		Dish that = (Dish) o;
+		return Double.compare(this.getCalories(), that.getCalories()) == 0;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(getCalories());
+	}
+
+	@Override
+	public String toString() {
+		return "Dish [id=" + id + ", name=" + name + ", calories=" + calories + ", kind=" + kind + ", vegetarian="
+				+ vegetarian + "]";
+	}
 }
--- lesson12-stream/src/main/java/stream/App.java
+++ lesson12-stream/src/main/java/stream/App.java
@@ -1,10 +1,15 @@
 package stream;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import bean.Dish;
+import bean.Dish.Kind;
 
 public class App {
 	public static void main(String[] args) {
@@ -18,14 +23,39 @@ public class App {
 			.collect(Collectors.toList())		// List<String>
 			.forEach(System.out::println);		// print
 		
+		// intermediate operation vs terminal operation
+		menu.stream()
+			.filter(d -> d.getCalories() > 300)
+			.map(Dish::getName)
+			.limit(2);
+		
+		// get MEAT dishes and make sure that there are no duplicates of calories
+//		List<Dish> meatDishes = menu.stream()	
+//							  .filter(d -> Kind.MEAT.equals(d.getKind()))
+//							  .distinct()
+//							  .collect(Collectors.toList());
+//		meatDishes.forEach(System.out::println);
+		
+		List<Dish> meatDishes = menu.stream()	
+				  .filter(d -> Kind.MEAT.equals(d.getKind()))
+				  .filter(distinctBy(Dish::getCalories))
+				  .collect(Collectors.toList());
+		meatDishes.forEach(System.out::println);
 	}
 	
 	private static List<Dish> getAll() {
 		return Arrays.asList(
-				new Dish("1", "pork", 308, false),
-				new Dish("2", "beef", 105, true),
-				new Dish("3", "chicken", 329, true),
-				new Dish("4", "dog", 302, false),
-				new Dish("5", "meomeo", 480, true));
+				new Dish("1", "D1", 308, Kind.FISH, false),
+				new Dish("2", "D2", 405, Kind.OTHER, true),
+				new Dish("3", "D3", 329, Kind.MEAT, true),
+				new Dish("4", "D4", 302, Kind.MEAT, false),
+				new Dish("5", "D5	", 480, Kind.OTHER, true),
+				new Dish("6", "D6", 329, Kind.MEAT, true),
+				new Dish("7", "D7", 302, Kind.MEAT, false));
+	}
+	
+	private static <T, R> Predicate<T> distinctBy(Function<T, R> func) {
+		Set<R> noDup = new HashSet<>();
+		return t -> noDup.add(func.apply(t));
 	}
 }
