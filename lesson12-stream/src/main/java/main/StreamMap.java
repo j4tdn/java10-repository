da1ce48b8a4fb2f5b15bb799fae477b7a7a53bd4
taquocthang <tathang0330@gmail.com>
2021-04-14 21:23:15 +0700

    lesson12-stream 14.04.2021

---
+++ lesson12-stream/src/main/java/main/StreamMap.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class StreamMap {
+	public static void main(String[] args) {
+		List<String> words = Arrays.asList("Java", "PHP", "Net", "Python");
+		Set<Integer> set = words.stream()
+			.map(String::length)	// 4 3 3 6 Stream<Integer>
+			.collect(Collectors.toSet());
+		
+		System.out.println("size: " + set.size());
+		
+		// get even digits => filter
+		// stream: 1 2 3 4 5 6 7 8
+		int[][] a2D = {{1,2,3}, {4,5}, {6,7,8}};
+		int[] evenNbrs = Arrays.stream(a2D)
+				.flatMapToInt(Arrays::stream)
+				.filter(t -> t % 2 == 0)
+				.toArray();
+		Arrays.stream(evenNbrs).forEach(System.out::println);
+	
+		List<List<String>> cards = Arrays.asList(
+			Arrays.asList("3 chuon", "2 co", "4 ro"),	
+			Arrays.asList("7 chuon", "3 co", "2 ro"),	
+			Arrays.asList("8 chuon", "4 co", "5 ro")
+			
+		);
+		
+		// filter cards are 2
+		// List<String>: 2 co, 2 ro
+		Set<String> cardResult = cards.stream()
+			.flatMap(Collection::stream)
+			.filter(c -> c.startsWith("2"))
+			.collect(Collectors.toSet());
+		cardResult.forEach(System.out::println);
+		
+	}
+	
+}
---
+++ lesson12-stream/src/main/java/main/StreamMatch.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class StreamMatch {
+	public static void main(String[] args) {
+		int[]  digits = { 1, 100, 35, 1, 99, 5, 8, 10, 77, 12 };
+		
+//		1st: is there any element that is divisible by 10
+		// anyMatch
+		boolean anyMatch = Arrays.stream(digits)
+			.anyMatch(digit -> digit % 10 == 0);
+		System.out.println("anyMatch: " + anyMatch);
+		
+//		2nd: find the first element that is divisible by 4
+		// find first
+		OptionalInt ex2 = Arrays.stream(digits) // int stream
+			.filter(digit -> digit % 4 == 0)
+			.findFirst();
+		if(ex2.isPresent()) {
+			System.out.println("VALUE: " + ex2.getAsInt());
+		}
+		
+//		3nd: find the element that is divisible by 5 in the list
+		// findAny
+		OptionalInt ex3 = Arrays.stream(digits)
+				.filter(digit -> digit % 5 == 0)
+				.findAny();
+		if(ex3.isPresent()) {
+			System.out.println("optInt3: " + ex3.getAsInt());
+		}
+		
+//		4th: does all elements less than 100
+		// allMatch
+		boolean allLessThan100 = Arrays.stream(digits)
+			.allMatch(digit -> digit < 100);
+			System.out.println("allLessThan100: " + allLessThan100);
+//		5th: does no elements greater than 0
+		// noneMatch
+		boolean noGreaterThan0 = Arrays.stream(digits)
+				.noneMatch(digit -> digit > 0);
+				System.out.println("noGreaterThan0: " + noGreaterThan0);
+	}
+}
