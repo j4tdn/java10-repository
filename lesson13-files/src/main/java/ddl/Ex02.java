1d45dd701e4d9bdbd0de311e6914baca119cc365
AnhTuan <anhtuannguyen331@gmail.com>
2021-04-25 20:20:28 +0700

    lesson13-files 25.04.2021

---
+++ lesson13-files/src/main/java/ddl/Ex02.java
@@ -0,0 +1,36 @@
+package ddl;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Random;
+
+import utils.FileUtils;
+
+public class Ex02 {
+	private static final String path = "random";
+
+	public static void main(String[] args) {
+		// createFiles("random", 20);
+		File[] roots = File.listRoots();
+		FileUtils.print(roots);
+
+		System.out.println("=====================");
+		File dir = new File(path);
+		File[] dirFiles = dir.listFiles();
+		FileUtils.print(dirFiles);
+
+		System.out.println("=====================");
+		File[] dirPdffiles = dir.listFiles(file -> file.isFile() && file.getName().endsWith("pdf"));
+		FileUtils.print(dirPdffiles);
+	}
+
+	public static void createFiles(String dirPath, int nofiles) {
+		Random rd = new Random();
+		String[] exts = { ".mp3", ".txt", ".pdf", ".bat" };
+		for (int i = 0; i < nofiles; i++) {
+			String path = dirPath + File.separator + "f" + i + exts[rd.nextInt(exts.length)];
+			FileUtils.createFile(path);
+		}
+
+	}
+}
