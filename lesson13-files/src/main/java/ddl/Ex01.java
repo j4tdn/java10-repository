379740d2705854fc8eabfdf13e2df8653440a4cf
tdhung239 <tdhung239@gmail.com>
2021-04-25 06:21:33 -0700

    lesson13-files 25.04.2021

---
+++ lesson13-files/src/main/java/ddl/Ex01.java
@@ -0,0 +1,15 @@
+package ddl;
+
+import java.io.File;
+
+import utils.FileUtils;
+
+public class Ex01 {
+	private static final String path = "lesson12\\data\\..\\Trader.txt";
+
+	public static void main(String[] args) {
+		File file = FileUtils.createFile(path);
+		// lastOfIndex("")
+		FileUtils.info(file);
+	}
+}
---
+++ lesson13-files/src/main/java/ddl/Ex02.java
@@ -0,0 +1,33 @@
+package ddl;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Random;
+
+import utils.FileUtils;
+
+public class Ex02 {
+	private static final String path = "random";
+
+	public static void main(String[] args) {
+		// createFiles("random",20);
+		File[] roots = File.listRoots();
+		FileUtils.print(roots);
+		System.out.println("======================");
+		File dir = new File(path);
+		File[] dirFiles = dir.listFiles();
+		FileUtils.print(dirFiles);
+		System.out.println("======================");
+		File[] dirPdfFiles = dir.listFiles(file->file.isFile() && file.getName().endsWith("pdf"));
+		FileUtils.print(dirPdfFiles);
+	}
+
+	private static void createFiles(String dirPath, int nofiles) {
+		Random rd = new Random();
+		String[] exts = { ".mp3", ".txt", ".bat", ".pdf" };
+		for (int i = 0; i < nofiles; i++) {
+			String path = dirPath + File.separator + "f" + i + exts[rd.nextInt(exts.length)];
+			FileUtils.createFile(path);
+		}
+	}
+}
