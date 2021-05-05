45d49e83f7e3ad03904f62307198e095581f1698
anhdao <anhdaodn0802@gmail.com>
2021-05-05 21:28:30 +0700

    lesson13-files 5.5.2021

---
+++ lesson13-files/src/main/java/dml/Ex01.java
@@ -0,0 +1,21 @@
+package dml;
+
+import java.io.File;
+
+import bean.Trader;
+import static utils.FileUtils.*;
+import utils.FileUtils;
+public class Ex01 {
+	private static final String path = "data\\Trader.txt";
+public static void main(String[] args) {
+	//String []student = {"Trader T7","Trader T8","Trader T9"};
+	
+	
+       File file = new File(path);
+//	FileUtils.writeLine(file, student);
+	print(FileUtils.readLines(file));
+	
+	Trader t1 = new Trader("Adam", "Milan");
+	writeLine(file,t1.toLine());
+}
+}
--- lesson13-files/src/main/java/utils/FileUtils.java
+++ lesson13-files/src/main/java/utils/FileUtils.java
@@ -1,18 +1,73 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FileUtils {
 	private FileUtils() {
 
 	}
 
+	public static void writeLine(File file, String...lines) {
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		try {
+			//open file -connection
+			fw = new FileWriter(file,true);
+			bw = new BufferedWriter(fw);
+			
+			//manipulate with file
+//			bw.newLine();
+//			bw.write("Line 2: Student A");
+			for(String student: lines) {
+				bw.newLine();
+				bw.write(student);
+				
+			}
+			//save and close
+			bw.close();
+			fw.close();
+			System.out.println("Write file "+ file .getName()+ "sucessful   !!");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close(fw);
+			close(bw);
+		}
+		
+	}
+	
+	public static List<String> readLines(File file){
+		
+		List<String> lines = new ArrayList<>();
+		FileReader fr = null;
+		BufferedReader br =null;
+		try {
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			String line =null;
+			while ((line = br.readLine()) != null) {
+				lines.add(line);
+			}
+		} catch ( IOException e) {
+			e.printStackTrace();
+		}finally {
+			close(br,fr);
+		}
+		return lines;
+	}
+	
 	public static File createFile(String path) {
 		File file = new File(path);
-		
-		
 		try {
 			if(!file.exists()) {
 			File parent =	file.getParentFile();
@@ -49,7 +104,17 @@ public class FileUtils {
 		}
 	}
 	
-	public static void print (File ...files) {
-		 Arrays.stream(files).forEach(f-> System.out.println(f.getAbsoluteFile()));
+	public static void print (List<String>list) {
+		 list.stream().forEach(f-> System.out.println(f));
+	}
+	private static void close(AutoCloseable ...closeables) {
+		Arrays.stream(closeables).forEach(c-> {
+			try {
+				c.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
 	}
 }
