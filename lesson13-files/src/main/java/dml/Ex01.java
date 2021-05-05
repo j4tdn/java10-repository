28b02d6efc53529f0e487b2d0fdb9a2eed294316
nhatTai <nhattaitran1999@gmail.com>
2021-05-05 21:27:58 +0700

    lesson13-files 05.05.2021

---
+++ lesson13-files/src/main/java/dml/Ex01.java
@@ -0,0 +1,24 @@
+package dml;
+
+
+import java.io.File;
+
+import bean.Trader;
+import static utils.FileUtils.*;
+
+public class Ex01 {
+	private static final String path = "data\\Trader.txt";
+	public static void main(String[] args) {
+		File file = new File(path);
+		
+		Trader t1 = new Trader("Adam", "Milan");
+		writeLines(file, t1.toLine());
+		
+		print(readLines(file));
+	// Student st1 = new Student(1 , "A", 10); => String s1 = "1, A, 10";
+		
+		// String line = "1, A , 10";
+		
+		//Student stLine = new Student(ar
+	}
+}
--- lesson13-files/src/main/java/utils/FileUtils.java
+++ lesson13-files/src/main/java/utils/FileUtils.java
@@ -1,22 +1,75 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FileUtils {
-	//private constructor
+	// private constructor
 	private FileUtils() {
-		
+
 	}
+
+	public static void writeLines(File file, String... lines) {
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		try {
+			// Open file - connection
+			fw = new FileWriter(file, true);
+			bw = new BufferedWriter(fw);
+
+			// manipulate with file
+			for (String line : lines) {
+				bw.newLine();
+				bw.write(line);
+			}
+
+			System.out.println("Write file" + file.getName() + " successful");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// close connection
+			close(bw, fw);
+		}
+	}
+
+	public static List<String> readLines(File file) {
+		FileReader fr = null;
+		BufferedReader br = null;
+		List<String> lines = new ArrayList<>();
+		try {
+			// Open file - connection
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			// manipulate with file
+			String line = null;
+			while((line = br.readLine()) != null) {
+				lines.add(line);				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// close connection
+			close(br, fr);
+		}
+		return lines;
+	}
+
 	public static File createFile(String path) {
 		File file = new File(path);
-		
+
 		try {
-			if(!file.exists()) {
+			if (!file.exists()) {
 				File parent = file.getParentFile();
 				System.out.println("parent: " + parent.getPath());
-				if(!parent.exists()) {
+				if (!parent.exists()) {
 					parent.mkdirs();
 				}
 				file.createNewFile();
@@ -27,18 +80,18 @@ public class FileUtils {
 		}
 		return file;
 	}
-	
+
 	public static File createDir(String path) {
 		File dir = new File(path);
 		dir.mkdirs();
 		return dir;
 	}
-	
+
 	public static boolean delete(String path) {
 		File file = new File(path);
 		return file.delete();
 	}
-	
+
 	public static void info(File file) {
 		System.out.println("path: " + file.getPath());
 		System.out.println("absolute path: " + file.getAbsolutePath());
@@ -48,9 +101,24 @@ public class FileUtils {
 			e.printStackTrace();
 		}
 	}
+
+	public static void print(File... files) {
+		Arrays.stream(files).forEach(f -> System.out.println(f.getAbsolutePath()));
+	}
 	
-	public static void print(File ...files) {
-		Arrays.stream(files)
-			.forEach(f -> System.out.println(f.getAbsolutePath()));
+	public static <E> void print(List<E> es) {
+		es.stream().forEach(System.out::println);
+	}
+	
+	
+
+	private static void close(AutoCloseable... closeables) {
+		Arrays.stream(closeables).forEach(c -> {
+			try {
+				c.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
