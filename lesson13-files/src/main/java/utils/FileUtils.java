af3024a864ad51eaf92613e1bbe2f4cf26a8e03c
tdhung239 <tdhung239@gmail.com>
2021-05-09 08:10:36 +0700

    lesson13-files 09.05.2021

--- lesson13-files/src/main/java/utils/FileUtils.java
+++ lesson13-files/src/main/java/utils/FileUtils.java
@@ -1,13 +1,67 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FileUtils {
 	// private constructor
 	private FileUtils() {
 
+	}
+
+	public static void writeLine(File file, String... lines) {
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		try {
+			// open file - connection
+			fw = new FileWriter(file, true);
+			bw = new BufferedWriter(fw);
+
+			// manipulate with file
+			for (String line : lines) {
+				bw.newLine();
+				bw.write(line);
+			}
+
+			System.out.println("Write file " + file.getName() + " sucessful !!!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// save and close
+			close(bw, fw);
+		}
+	}
+
+	public static List<String> readLine(File file) {
+		List<String> lines = new ArrayList<>();
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			// open file - connection
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+
+			// manipulate with file
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				lines.add(line);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// save and close
+			close(br, fr);
+		}
+		return lines;
 	}
 
 	public static File createFile(String path) {
@@ -19,9 +73,9 @@ public class FileUtils {
 					parent.mkdirs();
 				}
 				file.createNewFile();
-				System.out.println("file is reated successfully");
+				System.out.println("file is created successfully !!!");
 			}
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return file;
@@ -40,19 +94,29 @@ public class FileUtils {
 
 	public static void info(File file) {
 		System.out.println("path: " + file.getPath());
-		System.out.println("absolute: " + file.getAbsolutePath());
+		System.out.println("absolute path: " + file.getAbsolutePath());
 		try {
-			System.out.println("CanonicalPath: " + file.getCanonicalPath());
+			System.out.println("canonical path: " + file.getCanonicalFile());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public static void print(File ...files) {
-		Arrays.stream(files).forEach(f->System.out.println(f.getAbsolutePath()));
-	}
-	
-	
 
+	public static void print(File... files) {
+		Arrays.stream(files).forEach(f -> System.out.println(f.getAbsolutePath()));
+	}
+
+	public static <E> void print(List<E> es) {
+		es.stream().forEach(System.out::println);
+	}
+
+	private static void close(AutoCloseable... closeables) {
+		Arrays.stream(closeables).forEach(c -> {
+			try {
+				c.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
 }
