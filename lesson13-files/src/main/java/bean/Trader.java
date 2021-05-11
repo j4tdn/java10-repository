02df815926b94c55aba5c73e06ec9e9b24d109e6
taquocthang <tathang0330@gmail.com>
2021-05-11 10:04:57 +0700

    lesson13-files 11.05.2021

--- lesson13-files/src/main/java/bean/Trader.java
+++ lesson13-files/src/main/java/bean/Trader.java
@@ -1,6 +1,6 @@
 package bean;
 
-public class Trader {
+public class Trader implements FileData {
 	private final String name;
 	private final String city;
 
@@ -16,13 +16,22 @@ public class Trader {
 	public String getCity() {
 		return this.city;
 	}
-	
+
+	@Override
 	public String toLine() {
 		return city + ", " + name;
+	}
+
+	public static Trader transfer(String line) {
+		// Cambridge, Raoul
+		String[] tmps = line.split(", ");
+		if (tmps.length != 2) {
+			return null;
+		}
+		return new Trader(tmps[1], tmps[0]);
 	}
 
 	public String toString() {
 		return "Trader:" + this.name + " in " + this.city;
 	}
-
 }
