df5f7fbca0106ff00ac675e0d08b9876dc7a9e97
duonghuy1123 <duoonghuy1123@gmail.com>
2021-05-12 08:40:25 +0700

    lesson13-nguyenduonghuy 12/05/2020 update Ex05

--- lesson13-nguyenduonghuy/src/main/java/exercises/Ex05.java
+++ lesson13-nguyenduonghuy/src/main/java/exercises/Ex05.java
@@ -1,22 +1,52 @@
 package exercises;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
+import java.util.stream.Collectors;
+
 
 public class Ex05 {
-	private static Scanner sc = new Scanner(System.in);
+	private static final String path = "exercise 03" + File.separator + "input.txt";
 	private static Random rd = new Random();
 
-	public static void main(String[] args) {
-		File file = new File("input.txt");
+	public static void main(String[] args) throws IOException {
+		File file = createFile(path);
 
 		int[][] arr = getArray2D(5, 4);
 		pritnf(arr);
+		
+		List<String> data = toListString(arr);
+		writeData(file, data);
+	}
+	
+	private static File createFile(String path) {
+		File file = new File(path);
+		try {
+			if (!file.exists()) {
+				File parent = file.getParentFile();
+				if (!parent.exists()) {
+					parent.mkdirs();
+				}
+				file.createNewFile();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return file;
+	}
+	
+	private static void writeData(File file, List<String> data) {
+		try {
+			Files.write(Paths.get(file.getPath()), data);
+			System.out.println("Write file successfully!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private static int[][] getArray2D(int row, int col) {
@@ -30,6 +60,7 @@ public class Ex05 {
 	}
 	
 	private static void pritnf(int[][] arr) {
+		System.out.println("Array 2D: ");
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[0].length; j++) {
 				System.out.print(arr[i][j] + " ");
@@ -39,8 +70,14 @@ public class Ex05 {
 	}
 
 	private static List<String> toListString(int[][] arr) {
-		List<String> data = new ArrayList<>();
-		
-		return data;
+		String[] dataString = {"", "", "", "", ""};
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				dataString[i] += arr[i][j] + " ";
+			}
+		}
+		return Arrays.stream(dataString)
+					.map(d -> d.trim())
+					.collect(Collectors.toList());
 	}
 }
