a892d8d12ce0a8cfabd77106bd6618a51790a61f
phuquoc <quocduongphu@gmail.com>
2021-05-16 20:20:00 +0700

    lesson14-phuquoc 16.05.2021

---
+++ lesson14-exam-phuquoc/src/main/java/exercise/Ex02.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class Ex02 {
+	public static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.print("Nhập tuổi hiện tại của bạn: ");
+		int age = scanner.nextInt();
+		System.out.print("Nhập giờ đã ngủ: ");
+		
+		
+	
+		
+	}
+	
+}
---
+++ lesson14-exam-phuquoc/src/main/java/exercise/Ex04.java
@@ -0,0 +1,42 @@
+package exercise;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Ex04 {
+	public static final String path = "data\\student.txt";
+
+	public static void main(String[] args) {
+		File file = new File(path);
+		List<Student> Students = readFile(file.toPath());
+		Students.forEach(System.out::println);
+		System.out.println("1: Sắp xếp danh sách sinh viên tăng dần theo ĐTB.");
+		Students.stream().sorted(Comparator.comparing(dtb -> dtb.getPoint())).forEach(System.out::println);
+		System.out.println("2: Danh sách sinh viên có ĐTB > 8");
+		Students.stream().filter(dtb -> dtb.getPoint() > 8).forEach(System.out::println);
+		System.out.println("3: Sinh viên NỮ");
+		Students.stream().filter(gt -> gt.getSex().equals("Nu")).forEach(System.out::println);
+	}
+
+	private static List<Student> readFile(Path path) {
+		List<Student> result = new ArrayList<Student>();
+		try {
+			List<String> elements = Files.readAllLines(path);
+
+			for (String element : elements) {
+				String[] temp = element.split(", ");
+				Student item = new Student(temp[0], temp[1], Double.parseDouble(temp[2]), temp[3]);
+				result.add(item);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+}
