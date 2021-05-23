e9e3513a83c6af97b65c3f3e7b4939a0c74cb01e
anhdao <anhdaodn0802@gmail.com>
2021-05-23 20:53:14 +0700

    lesson15-thread 23.5.2021

---
+++ lesson15-thread/src/main/java/thread/Ex03.java
@@ -0,0 +1,17 @@
+package thread;
+
+public class Ex03 {
+public static void main(String[] args) {
+	System.out.println("thread main satrt");
+	Thread thread = new Thread(new Runnable() {
+		
+		@Override
+		public void run() {
+			System.out.println(Thread.currentThread().getName()+ " is running");
+
+		}
+	}, "Database - thread");
+	thread.start();
+	System.out.println("thread main end");
+}
+}
