665670983355b7f88b76e98e028d2e9edc119fd6
duonghuy1123 <duoonghuy1123@gmail.com>
2021-05-23 20:54:38 +0700

    lesson15-thread 23/05/2021

---
+++ lesson15-thread/src/main/java/thread/Ex01.java
@@ -0,0 +1,19 @@
+package thread;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println(Thread.currentThread().getName() + " is running ... ");
+		
+		// initial thread
+		Thread t0 = new Thread("t0") {
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName() + "  is running ... ");
+			}
+		};
+		t0.start();	// JVM automatically call run method
+		
+		
+		System.out.println("Thread main end");
+	}
+}
---
+++ lesson15-thread/src/main/java/thread/Ex03.java
@@ -0,0 +1,18 @@
+package thread;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		System.out.println("main start");
+		
+		Thread thread = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println(Thread.currentThread().getName() + "  is running ... ");
+			}
+		}, "database-thread");
+		thread.start();
+		
+		System.out.println("main end");
+	}
+}	
