c514e72a1bfc6f7b0b980dbdecd0dbf748e59b1c
taquocthang <tathang0330@gmail.com>
2021-07-11 11:26:35 +0700

    lesson17-quocthang 11.07.2021

---
+++ lesson17-quocthang/src/main/java/exercises/Ex02.java
@@ -0,0 +1,39 @@
+package exercises;
+
+import static utils.Utils.print;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.MatHang;
+import connection.DbConnection;
+
+public class Ex02 {
+	private static Connection connection = DbConnection.getConnection();
+
+	public static void main(String[] args) {
+		System.out.println("Liệt kê các mặt hàng theo mã loại và giá bán trung bình từ 100 đến 500 trong hệ thống");
+		List<MatHang> select = select(2, 100, 500);
+		print(select);
+	}
+
+	private static List<MatHang> select(Integer idCategory, Integer saleIn, Integer saleOut) {
+		List<MatHang> listItems = new ArrayList<>();
+		try {
+			Statement st = connection.createStatement();
+			ResultSet rs = st.executeQuery("SELECT * from mathang where maloai = " + idCategory + " and GiaBan BETWEEN "
+					+ saleIn + " and " + saleOut + "");
+			while (rs.next()) {
+				MatHang ItemGroup = new MatHang(rs.getInt("MaMH"), rs.getString("TenMH"), rs.getInt("GiaBan"));
+				listItems.add(ItemGroup);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listItems;
+	}
+}
