b6136f995b42f41b8b26638bf7ae6fd8c588dd1d
Quyen Phan <Quyen.Phan@mgm-tp.com>
2021-07-23 15:08:34 +0700

    Review - DangCongKhaiThu - 20/100 - FAILED

--- lesson17-dangcongkhaithu-exam/src/main/java/dao/ItemDao.java
+++ lesson17-dangcongkhaithu-exam/src/main/java/dao/ItemDao.java
@@ -21,7 +21,9 @@ public class ItemDao {
 	public ItemDao() {
 	}
 
+	// Câu 1: POINT: 10
 	public List<Item> getItemBySaleOut(LocalDate date) {
+		// Sử dụng JAVA07: List<T> result = new ArrayList<>();
 		List<Item> results = new ArrayList<Item>();
 		String query = "select mh.MaMH, mh.TenMH, dh.ThoiGianDatHang from MatHang mh join ChitietDonhang ctdh on mh.MaMH = ctdh.MaMH join DonHang dh on ctdh.MaDH = dh.MaDH\r\n"
 				+ "where dh.ThoiGianDatHang like '?%';";
@@ -29,12 +31,18 @@ public class ItemDao {
 		con = DBConnection.getConnection();
 		try {
 			ps = con.prepareStatement(query);
+			// Dòng 27: Chỗ  like '?%' em dùng ? như là 1 String thì
+			// Ở đây là phải là setString mới đúng. Code ko chạy được
+			
+			// Còn muốn setDate (java.sql.Date) thì trên kia phải là ThoiGianDatHang = 
+			// Yêu cầu where theo ngày nên chính xác là cast(ThoiGianDatHang as Date) = ?
 			ps.setDate(1, (Date) DateUtils.convertToDate(date));
 			rs = ps.executeQuery();
 			while (rs.next()) {
 				Item item = new Item();
 				item.setId(rs.getInt("MaMH"));
 				item.setName(rs.getString("TenMH"));
+				// Hàm cần tham số là LocalDate nhưng rs.getDate là java.sql.Date
 				item.setOrderTimne(DateUtils.convertToDate(rs.getDate("ThoiGianDathang")));
 
 				results.add(item);
@@ -46,6 +54,7 @@ public class ItemDao {
 		return results;
 	}
 
+	// Câu 3: 10
 	public List<String> getItemByAmount(int year) {
 		List<String> items = new ArrayList<String>();
 		String query = "select mh.TenMH from MatHang mh join  ChiTietDonHang ctdh on mh.MaMH = ctdh.MaMH join DonHang dh on dh.MaDH = ctdh.MaDH\r\n"
@@ -53,6 +62,9 @@ public class ItemDao {
 
 		try {
 			ps = con.prepareStatement(query);
+			// dh.ThoiGianDatHang like '%?%'
+			// Đây setInt thì trên kia phải là year(dh.ThoiGianDatHang) = ?
+			// Kết quả ko đúng
 			ps.setInt(1, year);
 			rs = ps.executeQuery();
 			while (rs.next()) {
