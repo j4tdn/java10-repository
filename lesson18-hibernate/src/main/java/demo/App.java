96afda6c79a82cd859d3c8caed06db7cfc3c8812
tdhung239 <tdhung239@gmail.com>
2021-07-25 22:51:20 +0700

    lesson18-hibernate 25.07.2021

---
+++ lesson18-hibernate/src/main/java/demo/App.java
@@ -0,0 +1,23 @@
+package demo;
+
+import java.util.List;
+
+import persistence.ItemGroup;
+import service.ItemGroupService;
+import service.ItemGroupServiceIml;
+
+public class App {
+	private static ItemGroupService itemGroupService;
+	static {
+		itemGroupService = new ItemGroupServiceIml();
+	}
+
+	public static void main(String[] args) {
+		List<ItemGroup> igrps = itemGroupService.getAll();
+		show(igrps);
+	}
+
+	private static <E> void show(List<E> element) {
+		element.forEach(System.out::println);
+	}
+}
